622e5382d33316912b3bfee6157561c1dfe7903a
pwj-v <melissa.velasco093@gmail.com>
2021-03-22 18:01:48 -0400

    removed the green background on divs as well as the numbers. Set a minimum width and height on divs.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let divContainer = document.querySelector('#container');
 for (i = 0; i < 256; i++) {
     let newDiv = document.createElement('div');
     newDiv.style.border = '1px solid black';
-    newDiv.style.backgroundColor = 'green';
-    newDiv.textContent = [i];
+    newDiv.style.minHeight = '35px';
+    newDiv.style.minWidth = '15px';
     newDiv.addEventListener('mouseover', () => {
         newDiv.style.backgroundColor = 'black';
     });
